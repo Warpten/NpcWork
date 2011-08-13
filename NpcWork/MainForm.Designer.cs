6fb64cec6f679201bd03c1a6c9f1d468b45cfae4
Kapoeira <vertozor@gmail.com>
2011-08-13 13:56:43 -0400

    Add untracked files from previous commit

---
+++ NpcWork/MainForm.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace NpcWork
+{
+    partial class mainWindow
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainWindow));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.DatabaseStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.BDDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.switchConnectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SeekButton = new System.Windows.Forms.Button();
+            this._npcIdToSeek = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.DatabaseStatusLabel});
+            resources.ApplyResources(this.statusStrip1, "statusStrip1");
+            this.statusStrip1.Name = "statusStrip1";
+            // 
+            // DatabaseStatusLabel
+            // 
+            this.DatabaseStatusLabel.Name = "DatabaseStatusLabel";
+            resources.ApplyResources(this.DatabaseStatusLabel, "DatabaseStatusLabel");
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.BDDToolStripMenuItem});
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // BDDToolStripMenuItem
+            // 
+            this.BDDToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectToolStripMenuItem,
+            this.switchConnectionToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.BDDToolStripMenuItem.Name = "BDDToolStripMenuItem";
+            resources.ApplyResources(this.BDDToolStripMenuItem, "BDDToolStripMenuItem");
+            // 
+            // connectToolStripMenuItem
+            // 
+            this.connectToolStripMenuItem.Name = "connectToolStripMenuItem";
+            resources.ApplyResources(this.connectToolStripMenuItem, "connectToolStripMenuItem");
+            this.connectToolStripMenuItem.Click += new System.EventHandler(this.SettingsClick);
+            // 
+            // switchConnectionToolStripMenuItem
+            // 
+            this.switchConnectionToolStripMenuItem.Name = "switchConnectionToolStripMenuItem";
+            resources.ApplyResources(this.switchConnectionToolStripMenuItem, "switchConnectionToolStripMenuItem");
+            this.switchConnectionToolStripMenuItem.Click += new System.EventHandler(this.DBConnect);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            resources.ApplyResources(this.exitToolStripMenuItem, "exitToolStripMenuItem");
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.groupBox1);
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Controls.Add(this.SeekButton);
+            this.groupBox1.Controls.Add(this._npcIdToSeek);
+            this.groupBox1.Controls.Add(this.label1);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // SeekButton
+            // 
+            resources.ApplyResources(this.SeekButton, "SeekButton");
+            this.SeekButton.Name = "SeekButton";
+            this.SeekButton.UseVisualStyleBackColor = true;
+            this.SeekButton.Click += new System.EventHandler(this.DoSeekNPC);
+            // 
+            // _npcIdToSeek
+            // 
+            resources.ApplyResources(this._npcIdToSeek, "_npcIdToSeek");
+            this._npcIdToSeek.Name = "_npcIdToSeek";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // mainWindow
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "mainWindow";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem BDDToolStripMenuItem;
+        private System.Windows.Forms.ToolStripStatusLabel DatabaseStatusLabel;
+        private System.Windows.Forms.ToolStripMenuItem connectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem switchConnectionToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button SeekButton;
+        private System.Windows.Forms.TextBox _npcIdToSeek;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ NpcWork/NpcEnums.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace NpcWork
+{
+    /// <summary>
+    /// A NPC's DB Template
+    /// </summary>
+    public static readonly struct NpcTemplate
+    {
+        uint entry;
+        fixed uint difficultyEntry[3];
+        fixed uint killCredit[2];
+        fixed uint modelID[4];
+        string name;
+        string subname;
+        string iconName;
+        uint gossipMenuId;
+        uint minLevel;
+        uint maxLevel;
+        uint expansion;
+        fixed uint faction[2];
+        uint npcFlag;
+        fixed float speed[2];
+        float scale;
+        uint rank;
+        fixed float dmg[2];
+        uint dmgSchool;
+        uint attackPower;
+        float dmgMultiplier;
+        uint baseAttackTime;
+        uint rangeAttackTime;
+        uint unitClass;
+        uint unitFlags;
+        uint dynamicFlags;
+        uint family;
+        uint trainerType;
+        uint trainerSpell;
+        uint trainerClass;
+        uint trainerRace;
+        fixed float rangeDmg[2];
+        uint rangedAttackPower;
+        uint type;
+        uint typeFlags;
+        uint lootID;
+        uint pickPocketLoot;
+        uint skinLoot;
+        fixed uint resistance[6];
+        fixed uint spell[8];
+        uint PetSpellDataId;
+        uint VehicleID;
+        fixed uint gold[2];
+        string AIName;
+        uint movementType;
+        uint inhabitType;
+        float healthMod;
+        float manaMod;
+        float armorMod;
+        uint racialLeader;
+        fixed uint QuestItem[6];
+        uint movementID;
+        uint regenHealth;
+        uint equipmentID;
+        uint MechanicImmuneMask;
+        uint flagsExtra;
+        string ScriptName;
+    };
+}
